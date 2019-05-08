ebb5ac65c2367e4c2ef79b25641d19e8b5a010d5
An Nguyen <nguyenvanan808@gmail.com>
2019-05-08 17:48:38 +0700

    vs1

--- app/src/main/java/com/example/mykotlin1/MainActivity.kt
+++ app/src/main/java/com/example/mykotlin1/MainActivity.kt
@@ -2,18 +2,57 @@ package com.example.mykotlin1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item->
+        when(item.itemId){
+            R.id.trangchu ->{
+                //item.setIcon(R.drawable.tab1_active)
+                Log.d("trangchu","Man hinh trang chu")
+                replaceFragment(trangchu_Fragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.lichsu ->{
+                Log.d("trangchu","Man hinh lich su")
+                replaceFragment(lichsu_Fragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.hotro ->{
+                Log.d("trangchu","Man hinh ho tro")
+                replaceFragment(hotro_Fragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.game ->{
+                Log.d("trangchu","Man hinh Game")
+                replaceFragment(game_Fragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.taikhoan ->{
+                Log.d("trangchu","Man hinh tai khoan")
+                replaceFragment(taikhoan_Fragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            else -> return@OnNavigationItemSelectedListener  false
+        }
 
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Sinhvien("Nguyễn Văn An")
-        tvShow.text="Chào Kotlin"
+        replaceFragment(trangchu_Fragment())
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        bottomNavigation.setItemIconTintList(null);
+        //val menu = bottomNavigation.menu;
+        //menu.findItem(R.id.trangchu).setIcon(R.drawable.tabhome);
+
     }
- fun Sinhvien(name:String){
-     Log.d("123",name)
- }
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer,fragment)
+        fragmentTransaction.commit()
+    }
 }
